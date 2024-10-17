212b3ef595b1bce5f9c52bd92b4ccacc3280c425
ifme18 <mainakenken188@gmail.com>
2024-10-17 10:42:21 +0300

    passed all

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())  // 
+    .then(resp => resp.json()) 
+    .then(json => renderBooks(json));  //  fetching json and rendering the books intidoc
   
 }
 
